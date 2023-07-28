5e7b55bbf7e28b59bcf661b68123584341449141
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-28 15:58:06 -0400

    fetching the user from an api

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,8 +3,15 @@ import "./App.css";
 
 class App extends Component {
   constructor() {
-    //its method to initialize an objects state in a class. it automatically calls during the creation of an object in a  class.
-    /*
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  //its method to initialize an objects state in a class. it automatically calls during the creation of an object in a  class.
+  /*
 The constructor in react is called before the component is mounted.
 When you implement the constructor for a React Component , you need to call super(props) method before any other statements.
 
@@ -18,21 +25,30 @@ Constructor (props) {
 2. It used for binding event handler methods that occur in your component.
 
 */
-    super();
 
-    this.state = {
-      monsters: [{ name: "Lynda", id: 1234 }, { name: "Frank", id: 3522 }, { name: "Jacky",id: 21232 }, {name:"jay" , id: 242343}],
-    };
+
+
+//Fetching the API 
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) =>response.json())
+      .then(
+        (users) =>
+          this.setState(() => {
+            return { monsters: users };
+          }),
+        () => {
+          console.log(this.state);
+        }
+      );
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>; //Key Properties: 
-          //most of the time key would be unique id ,
-
-        })}
+        {this.state.monsters.map((monster) => {  return <h1 key={monster.id}>{monster.name}</h1>;})}
+ 
       </div>
     );
   }
