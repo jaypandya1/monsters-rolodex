3ce99cfa4f1433c30a5c8c799a74ca97c983e6dc
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-28 12:47:22 -0400

    state updates

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,12 +1,25 @@
+import { Component } from "react";
+import "./App.css";
 
-import './App.css';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+    this.state = {
+      name: "dada",
+      company:'qwe'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>Hello {this.state.name} , I want to be {this.state.company}</p>
+        <button onClick={() => this.setState({ name: "jay" })}>
+          Change Name
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
