e772fb79e5254ee5a766f6762bb9e786d9bfc56c
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-28 15:00:38 -0400

    learned concpet for keys , map ()

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -21,7 +21,7 @@ Constructor (props) {
     super();
 
     this.state = {
-      monsters: [{ name: "Lynda" }, { name: "Frank" }, { name: "Jacky" }, {name:"jay"}],
+      monsters: [{ name: "Lynda", id: 1234 }, { name: "Frank", id: 3522 }, { name: "Jacky",id: 21232 }, {name:"jay" , id: 242343}],
     };
   }
 
@@ -29,7 +29,9 @@ Constructor (props) {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>; //Key Properties: 
+          //most of the time key would be unique id ,
+
         })}
       </div>
     );
@@ -37,3 +39,21 @@ Constructor (props) {
 }
 
 export default App;
+
+/*
+map()
+
+will use to modify arr 
+
+exp: 
+
+const myArray = [1,2,3,4]
+
+myArray.map( e  => {   return e + 1     }       )
+
+
+will add 1 in each element of myArray
+
+
+
+*/
