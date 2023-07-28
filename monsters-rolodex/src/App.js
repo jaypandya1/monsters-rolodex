ee0fa9caaa1b6a3aeb115fda0e2ab015ce10ab49
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-28 14:33:42 -0400

    adding notes for constructor and map()

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,20 +3,34 @@ import "./App.css";
 
 class App extends Component {
   constructor() {
+    //its method to initialize an objects state in a class. it automatically calls during the creation of an object in a  class.
+    /*
+The constructor in react is called before the component is mounted.
+When you implement the constructor for a React Component , you need to call super(props) method before any other statements.
+
+##Syntax : 
+Constructor (props) {
+  super(props);
+}
+
+##Uses of constructors ::
+1. It used for initializing the local state of the component by assigning an object to this.state
+2. It used for binding event handler methods that occur in your component.
+
+*/
     super();
 
     this.state = {
-      name: "dada",
-      company:'qwe'
+      monsters: [{ name: "Lynda" }, { name: "Frank" }, { name: "Jacky" }, {name:"jay"}],
     };
   }
+
   render() {
     return (
       <div className="App">
-        <p>Hello {this.state.name} , I want to be {this.state.company}</p>
-        <button onClick={() => this.setState({ name: "jay" })}>
-          Change Name
-        </button>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
