8d905d1726f3145a02116246f934cde86ae65326
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-31 13:32:55 -0400

    adding name email img and prestyled css files

--- monsters-rolodex/src/componets/card-list/card-list.component.jsx
+++ monsters-rolodex/src/componets/card-list/card-list.component.jsx
@@ -1,20 +1,31 @@
 import { Component } from "react";
 
+import './card-list.style.css';
 class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+    console.log(this.props);
+    return (
+      <div className="Card-List">
+        {monsters.map((monster) => {
+            const {name,email,id } = monster;
+        return (
+          <div className="card-container" key={monster.id}>
+            <img
+              alt={`monster ${monster.name}`}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            />
 
-render() {
+            <h2 key={id}>{name}</h2>
+            <p>{email}</p>
+          </div>
 
-    const {monsters} = this.props;
-console.log(this.props);
-return <div>{monsters.map(monster => (
+        )})}
 
-    <h1 key={monster.id}>{monster.name}</h1>
-) )}</div>;
-
-
-}
-
-
-
+      </div>
+    );
+  }
 }
 export default CardList;
+
+//React rerender when setState and props change
