f4b9e3a6b34774de5c75e82fe6157733ff5a0eb2
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2023-07-31 10:11:31 -0400

    day2

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,28 +7,10 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      Searchfield: ''
+      Searchfield: "",
     };
     console.log("component");
   }
-
-  //its method to initialize an objects state in a class. it automatically calls during the creation of an object in a  class.
-  /*
-The constructor in react is called before the component is mounted.
-When you implement the constructor for a React Component , you need to call super(props) method before any other statements.
-
-##Syntax : 
-Constructor (props) {
-  super(props);
-}
-
-##Uses of constructors ::
-1. It used for initializing the local state of the component by assigning an object to this.state
-2. It used for binding event handler methods that occur in your component.
-
-*/
-
-  //Fetching the API
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -45,29 +27,30 @@ Constructor (props) {
     console.log("componentdidmount");
   }
 
-  render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.Searchfield);
-      
+  onSearchChange(event) {
+    const Searchfield = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { Searchfield };
     });
+  }
+
+  render() {
+    const { monsters, Searchfield } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(Searchfield);
+    });
+
     console.log("render");
     return (
-    
       <div className="App">
         <input
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            console.log(event.target.value);
-     const Searchfield = event.target.value.toLowerCase();
-          
-
-            this.setState(() => {
-              
-              return { Searchfield};
-            });
-          }}
+          onClick={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
@@ -78,21 +61,3 @@ Constructor (props) {
 }
 
 export default App;
-
-/*
-map()
-
-will use to modify arr 
-
-exp: 
-
-const myArray = [1,2,3,4]
-
-myArray.map( e  => {   return e + 1     }       )
-
-
-will add 1 in each element of myArray
-
-
-
-*/
